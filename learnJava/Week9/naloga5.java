cd8386ba1e774868fb6aad86fb28d22c0ba52108
primi9758 <zidansekprimoz@gmail.com>
2021-09-01 09:34:16 +0200

    replaced character.isLetter() with a conversion to an int type and then check on ascii table if the value represents a character

--- learnJava/Week9/naloga5.java
+++ learnJava/Week9/naloga5.java
@@ -1,14 +1,13 @@
-import java.util.*;
+import java.util.Scanner;
 public class naloga5 {
     public static void main (String[] args){
         Scanner sc = new Scanner(System.in);
         char what = sc.next().charAt(0);
-        boolean b = Character.isLetter(what);
-        if (b == false) {
-            System.out.println("NO");
-        } else if (b == true) {
+        int asciiVAlue = (int) what;
+        if (asciiVAlue >= 65 && asciiVAlue <= 90 || asciiVAlue >= 97 && asciiVAlue <= 122) {
             System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
-
     }
 }
